dcda8ac72c96133839d24ea5131f8f5e3a1b4e74
Nikolai Grigorje <topclash765@gmail.com>
2021-10-19 23:28:36 +0300

    ff

--- ExGrigorjev/Program.cs
+++ ExGrigorjev/Program.cs
@@ -51,16 +51,24 @@ namespace ExGrigorjev
 
                 }
                 Console.WriteLine(num);
-                
 
+            Console.WriteLine("--------------------------------Nr------------------------------------------");
+            /*Joosepi probleem. Ringis on n inimest. Juht loeb ringis, alustades esimesest, ja kuvab ("täidab") m-nda inimese. 
+            Ring on suletud, loendamist jätkatakse pärast "hukatud" järgmisest; see kestab seni, kuni ainult üks inimene jääb "ellu". 
+            Leidke ellujäänud isiku number, samuti leidke selline m> 1, mille juures esimene inimene jääb "elavaks" [10].*/
 
-            Console.WriteLine("--------------------------------Nr3------------------------------------------");
-            Random rann = new Random();
-            int[] P = new int[10];
-            int kaznit = 0;
-            for (int i = 0; i < 10; i++)
+            Console.Write("Kui palju mängijaid peaksin lisama?");
+            int n = int.Parse(Console.ReadLine());
+            var lst = Enumerable.Range(1, n).ToList();
+            Console.Write("Mis arvele?");
+            int m = int.Parse(Console.ReadLine());
+            int curNum = 0;
+            Console.WriteLine(string.Join(" ", lst));
+            while (lst.Count > 1)
             {
-                
+                curNum = (curNum + m) % lst.Count;
+                lst.RemoveAt(curNum);
+                Console.WriteLine(string.Join(" ", lst));
             }
             Console.ReadLine();
 
